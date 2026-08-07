95515a2db06792753ecc6e72a05f81fc3eb56cf2
devGZkt <gabriel.zimmermann.dev@gmail.com>
2026-08-07 20:35:10 +0200

    Remove accidentally duplicated Documents/Code/Velora nested directory

--- Documents/Code/Velora/Backend/Models/DTOs/CreateCategoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend.Models.DTOs
-{
-    public class CreateCategoryDto
-    {
-        public required string Slug { get; set; }
-        public bool Active { get; set; }
-        public int DisplayOrder { get; set; }
-    }
-}
